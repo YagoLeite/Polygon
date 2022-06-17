da9ca534ff7131421f9c4db7b08c1a3e1616b294
Yago Batista <yagobatistafis@yahoo.com.br>
2022-06-17 17:54:34 -0300

    adding logic to position the divs

--- components/Body/Wrapper.js
+++ components/Body/Wrapper.js
@@ -2,6 +2,28 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 import { useControlerContext } from "../../Context/Context";
 
+const findingAngles = (number) => {
+  const interiorAngle = (360 / number) * (Math.PI / 180);
+  let arrayOfAngles = [];
+  for (let i = 1; i <= number; i++) {
+    arrayOfAngles.push(i * interiorAngle);
+  }
+  return arrayOfAngles;
+};
+const arrayOfPositions = (array) => {
+  return array.map((angle) => {
+    return { x: Math.sin(angle) / 2, y: Math.cos(angle) / 2 };
+  });
+};
+const topAndLeft = (array) => {
+  return array.map((position) => {
+    return {
+      left: (0.5 - position.x) * 100,
+      top: (0.5 - position.y) * 100,
+    };
+  });
+};
+
 const Wrapper = () => {
   const { state } = useControlerContext();
   console.log(state);
