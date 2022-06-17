0fff49bf24fd0995843c4ba5bd21d9e5a75e3e05
Yago Batista <yagobatistafis@yahoo.com.br>
2022-06-17 18:04:54 -0300

    helper functions

---
+++ helpers/Helpers.js
@@ -0,0 +1,21 @@
+export const findingAngles = (number) => {
+  const interiorAngle = (360 / number) * (Math.PI / 180);
+  let arrayOfAngles = [];
+  for (let i = 1; i <= number; i++) {
+    arrayOfAngles.push(i * interiorAngle);
+  }
+  return arrayOfAngles;
+};
+export const arrayOfPositions = (array) => {
+  return array.map((angle) => {
+    return { x: Math.sin(angle) / 2, y: Math.cos(angle) / 2 };
+  });
+};
+export const topAndLeft = (array) => {
+  return array.map((position) => {
+    return {
+      left: (0.5 - position.x) * 100,
+      top: (0.5 - position.y) * 100,
+    };
+  });
+};
