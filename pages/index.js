a3b0282e3522965a78783096f730f3da46dcdf78
Yago Batista <yagobatistafis@yahoo.com.br>
2022-06-17 18:22:46 -0300

    ?

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import Wrapper from "../components/Body/Wrapper";
 import Controler from "../components/Controler/Controler";
+import { useControlerContext } from "../Context/Context";
 
 export default function Home() {
   return (
-    <>
+    <Box>
       <Controler />
       <Wrapper />
-    </>
+    </Box>
   );
 }
