b91a772dfc0afdc92192d96e69ba71be66222976
Yago Leite <yagobatistafis@yahoo.com.br>
2022-06-21 15:13:28 -0300

    changing initial state

--- context/ControlerContext.js
+++ context/ControlerContext.js
@@ -6,8 +6,8 @@ const Controler = createContext();
 const ControlerState = ({ children }) => {
   const [state, dispatch] = useReducer(controlerReducer, {
     sides: 2,
-    divSize: 300,
-    verticeDiv: 50,
+    divSize: 500,
+    verticeDiv: 4,
     color: "#712B75",
     fadeInAnim: false,
     bigDivRotationAnim: false,
